d355a05042c1d370ce72020686a3d746c3090acf
Guilherme Aureliano <49173189+GuilhermeAureliano@users.noreply.github.com>
2023-02-13 13:23:41 -0300

    Project created

---
+++ backend/src/test/java/com/dev/scrumboard/ScrumboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.scrumboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
